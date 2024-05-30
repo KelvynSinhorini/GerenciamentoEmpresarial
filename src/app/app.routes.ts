6bc8e82f3930e8aadcb5bd20710a404fde6d4272
Kelvyn Sinhorini <kelvynsinhorini03@gmail.com>
2024-05-30 14:37:39 -0300

    fet: add fornecedor-list componente e ajustar a rota para esse componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FornecedorListComponent } from './components/fornecedor/fornecedor-list/fornecedor-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'fornecedores', component: FornecedorListComponent }
+];
